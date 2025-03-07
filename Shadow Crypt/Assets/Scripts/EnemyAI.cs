f8bea9f425444ad9a3ae72bc1b8d679db475301a
Neeraj-Gulavani <neerajgulavani@gmail.com>
2025-03-07 13:32:50 +0530

    Added Enemy AI and Attack

---
+++ Shadow Crypt/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform player;
+    private Rigidbody2D rb;
+
+    public float speed;
+    public float detectRange;
+    public float shootRange;
+    public GameObject bullet;
+    public float fireRate;
+    public float nextFireTime;
+    public Transform firePt;
+    public float bulletSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player==null) return;
+        float playerDistance=Vector2.Distance(transform.position,player.position);
+        if (playerDistance<=detectRange) {
+
+            if (playerDistance<shootRange) {
+                rb.velocity=Vector2.zero;
+                if (Time.time>=nextFireTime) {
+                    Shoot();
+                    nextFireTime = Time.time+fireRate;
+                }
+            } else {
+                MoveToPlayer();
+            }
+
+        }
+    }
+
+    void MoveToPlayer() {
+        Vector2 dir = (player.position-transform.position).normalized;
+        rb.velocity=dir*speed;
+    }
+
+    void Shoot() {
+        if (player==null || firePt==null) return;
+        Vector2 dir = (player.position-firePt.position).normalized;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion rot = Quaternion.Euler(0,0,angle);
+        GameObject bulletObj = Instantiate(bullet,firePt.position,rot);
+        Rigidbody2D bulletRb = bulletObj.GetComponent<Rigidbody2D>();
+        
+        bulletRb.velocity = dir*bulletSpeed;
+
+    }
+}
---
+++ Shadow Crypt/Assets/Scripts/bullet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float life=3f;
+    public float damage=10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject,life);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            PlayerHealth ph =other.GetComponent<PlayerHealth>();
+            if (ph!=null) {
+                ph.TakeDamage(damage);
+            }
+        }
+    }
+}
