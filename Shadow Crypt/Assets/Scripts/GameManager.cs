c0cd26d993b37d7d5e739210b87c1707b937d2e0
Neeraj-Gulavani <neerajgulavani@gmail.com>
2025-03-10 01:52:25 +0530

    Added Pause Menu and Updated Build

--- Shadow Crypt/Assets/Scripts/GameManager.cs
+++ Shadow Crypt/Assets/Scripts/GameManager.cs
@@ -8,27 +8,50 @@ public class GameManager : MonoBehaviour
     public Transform spawnPos;
     public SpriteRenderer sr,sr1,sr2;
     public GameObject deathui;
+    public GameObject pauseui;
+    public bool ispaused=false;
     public void Respawn() {
         
      SceneManager.LoadScene(SceneManager.GetActiveScene().name);   
     }
     public void Quit() {
-        Application.Quit();
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale=1;
     }
 
     public void GotoMainMenu() {
-             SceneManager.LoadScene("MainMenu");   
+             SceneManager.LoadScene("MainMenu");
+             Time.timeScale=1;   
 
     }
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        ispaused=false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            TogglePause();
+        }
     }
+
+    public void TogglePause() {
+        if (ispaused) {
+                Time.timeScale=1;
+                pauseui.SetActive(false);
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            else {
+                Time.timeScale=0;
+                pauseui.SetActive(true);
+                Cursor.lockState = CursorLockMode.Confined;
+            }
+            ispaused=!ispaused;
+    }
+
 }
+
