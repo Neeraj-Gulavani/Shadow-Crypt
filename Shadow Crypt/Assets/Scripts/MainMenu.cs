ee544402a3c1f9e4a33bbab517eb4ddc368e5061
Neeraj-Gulavani <neerajgulavani@gmail.com>
2025-03-10 00:43:05 +0530

    Added Main Menu

--- Shadow Crypt/Assets/Scripts/MainMenu.cs
+++ Shadow Crypt/Assets/Scripts/MainMenu.cs
@@ -16,7 +16,7 @@ public class MainMenu : MonoBehaviour
     }
 
     public void Quit() {
-        Application.Quit();
+        SceneManager.LoadScene("MainMenu");
     }
     // Start is called before the first frame update
     void Start()
